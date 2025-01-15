663bc2a3c55f48f6fa7149acf02c018a4136f6f9
tiresomefanatic <30657622+tiresomefanatic@users.noreply.github.com>
2025-01-15 19:02:29 +0530

    app.vue changed

--- app.vue
+++ app.vue
@@ -1,6 +1,32 @@
+# app.vue
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtLayout>
+      <Header />
+      <main class="mt-24">
+        <NuxtLoadingIndicator />
+        <NuxtPage />
+      </main>
+    </NuxtLayout>
   </div>
 </template>
+
+<script setup>
+import Header from "~/components/Header.vue";
+</script>
+
+<style>
+body {
+  @apply bg-white;
+}
+
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.2s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
